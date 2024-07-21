01d5a4f1aae49931e92589b694197a7bad23f940
matus <bartko.mat@gmail.com>
2024-07-21 19:19:46 +0200

    merge modelina generator to main asyncapi generator

--- libs/flea/asyncapi-generator-typescript-cdk-event-bridge/src/index.tsx
+++ libs/flea/asyncapi-generator-typescript-cdk-event-bridge/src/index.tsx
@@ -1,4 +1,5 @@
 import { File } from '@asyncapi/generator-react-sdk';
+import {FormatHelpers, TypeScriptGenerator} from '@asyncapi/modelina';
 import {FleaFileEventBridgeRule} from './lib/flea-asyncapi-generator-typescript-cdk-event-bridge-rule';
 import { kebabCase } from 'change-case-all';
 
@@ -9,23 +10,33 @@ export default async function ({asyncapi}) {
   const domainName = asyncapi.info().title();
   const fileNameFleaFileEventBridgeRule = `${kebabCase(domainName)}-event-bridge-rule.construct.ts`;
 
+  const generator = new TypeScriptGenerator({
+    modelType: "interface"
+  });
+
+  const models = await generator.generate(asyncapi);
+  const modelFileContent = models.map(outputModel => editFileContent(outputModel.result))
+    .join("\r\n\r\n");
+  /*
+  const modelFiles = [];
+  for (const model of models) {
+    // 8
+    const modelFileName = `${FormatHelpers.toParamCase(model.modelName)}.ts`;
+    // 9
+    modelFiles.push(<File name={modelFileName}>{model.result}</File>);
+  }
+*/
   return [
       <File name={fileNameFleaFileEventBridgeRule}>
         <FleaFileEventBridgeRule domainName={domainName} asyncapi={asyncapi}></FleaFileEventBridgeRule>
       </File>,
-    ];
-  /*
-  const schemas = asyncapi.allSchemas();
-
-
-  return Array.from(schemas).map(([schemaName, schema]) => {
-    console.debug(schemaName, JSON.stringify(schema, null, 4));
-    const name = kebabCase(schemaName);
-    return (
-      <File name={`${name}.json`}>
-        {JSON.stringify(schema, null, 4)}
-      </File>
-    );
-  });*/
+    <File name="models.ts">{modelFileContent}</File>
+    ]
 
 }
+
+const editFileContent = (fileContent) => {
+  return `export ${fileContent}`;
+}
+
+
